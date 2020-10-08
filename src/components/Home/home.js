937a0dd0e8422d173bd1a39ae3ee9e2b1144503d
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-10-08 17:46:53 -0400

    dash fix

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -6,7 +6,6 @@ function Home() {
     <div className="home">
         <h1 className='title-1'>Welcome To</h1>
         <h1 className='title-2'>React Games</h1>
-        <h3 className='title-3'>by Jesus Guerrero</h3>
         <p className='title-text'>This is an app I made to serve as an archive for all the different React games I build.<br/>I'll continue to add more games as time allows. Have fun!</p>
         <a className='portfolio-link' href='http://www.jesusguerrero.dev'>My Website</a>
     </div>
